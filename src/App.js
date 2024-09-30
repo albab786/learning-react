b24e9ceb35f04b586533bed0d78e7d464f338217
Albab <116362742+albab786@users.noreply.github.com>
2024-09-30 21:22:05 +0530

    today's commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <Header></Header>
+    
+//   )
+// }
 
-export default App;
+// export default App;
+
+//class based components
+import { Component } from "react";
+ class App extends Component {
+  render(){
+    return <h1> I am class based components</h1>
+  }
+ }
+ export default App;
+  
